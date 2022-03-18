4f318d0051e8453099a8bb545d6dfaffac6d8f56
Marcel46509 <88722912+Marcel46509@users.noreply.github.com>
2022-03-18 16:52:24 +0100

    Update UILibrary.lua

--- UILibrary.lua
+++ UILibrary.lua
@@ -2,6 +2,7 @@
 local TweenService, RunService, UserInputService,gui,dragging,dragInput,dragStart,startPos,cpt,cpf,cppicking,cppickingVal,cppickingAlpha,cphue,cpsat,cpval,focused,highest,focusedBox = game:GetService("TweenService"),game:GetService("RunService"), game:GetService("UserInputService")
 local cpalpha = 0
 local GuiName2137 = "Cuteware"
+
 -- Dragging
 local function updateDrag(input)
     local delta = input.Position - dragStart
